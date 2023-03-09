a0b8df7a38eb05650ddbf67f8840c9d9be63ec3c
Margad <bmargad17@gmail.com>
2023-03-09 15:25:33 +0800

    Update commentdata.js

--- src/Data/commentdata.js
+++ src/Data/commentdata.js
@@ -26,6 +26,13 @@ const data =  [
     "comment": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industr 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled t to make a type specimen book. It has survived not only five centuries, but also the leap into electro ic typesetting, remaining essentially unchanged",
     "star": "5",
     "bookId": "3"
+  },
+  {
+    "commentId": "5",
+    "userId": "Hello World",
+    "comment": "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industr 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled t to make a type specimen book. It has survived not only five centuries, but also the leap into electro ic typesetting, remaining essentially unchanged",
+    "star": "3",
+    "bookId": "4"
   }
 ]
 
